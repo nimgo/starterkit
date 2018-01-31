67098b56619e4352be15722a33b1834b98b6bd06
javantwm <noreply@munchpress.com>
2018-02-01 07:45:43 +0800

    mockapi

---
+++ experiment/mock/mock-server.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const jsonServer = require('json-server')
+
+// Create the mock server
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+// // Add custom routes before JSON Server router
+// server.get('/test', (req, res) => {
+//     res.jsonp(req.query)
+// })
+
+// // To handle POST, PUT and PATCH you need to use a body-parser
+// // You can use the one used by JSON Server
+// server.use(jsonServer.bodyParser)
+// server.use((req, res, next) => {
+//   if (req.method === 'POST') {
+//     req.body.createdAt = Date.now()
+//   }
+//   // Continue to JSON Server router
+//   next()
+// })
+
+server.use('/api', router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
